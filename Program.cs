26c2b9cff11d248743e380d3cd348cfa95d92453
Владислав Персаев <perssskk@mail.ru>
2025-11-26 17:51:19 +0300

    выполнил задание 1, 2, 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +11,56 @@ namespace _12345
     {
         static void Main(string[] args)
         {
-            string[] question = { "четрые или пять?", "сколько ног у осьминога", "сколько пар завтра", "сколько время", "как ты?", "нормас" };
-            string[] answer = { "5", "7", "4", "9:52", "норма", "ое" };
-            int countCorrectAnswer = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                
-                int randomQuest = new Random().Next(0, question.Length);
 
-                Console.WriteLine(question[randomQuest]);
+            string[] question = { "четрые или пять?", "сколько ног у осьминога", "сколько пар завтра", "сколько время", "gg" };
+            string[] answer = { "5", "7", "4", "9:52", "dobeg" };
+
+            int countCorrectAnswer = 0;
+
+            Console.WriteLine("Как вас зовут?");
+
+            string nameUser = Console.ReadLine();
+
+            Console.WriteLine($"Отлично, {nameUser} ,отвечай на вопросы, и узнаешь свой диагноз");
+
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++) // сам тест 
+            {
+                Console.WriteLine(question[i]);
                 string userAnswer = Console.ReadLine();
-                if (question[randomQuest] == userAnswer)
+                if (answer[i] == userAnswer)
                 {
                     countCorrectAnswer++;
                 }
             }
-            Console.WriteLine(countCorrectAnswer);
+            string Diagnosis = DefinitionDiagnosis(countCorrectAnswer);
+            Console.WriteLine();
+            Console.WriteLine($"Количество баллов: {countCorrectAnswer}");
+            Console.WriteLine($"{nameUser}, твой диагноз: {Diagnosis}");
+
         }
+        
+
+
+
+        static string DefinitionDiagnosis(int countCorrectAnswer)
+        {
+            string diagnoz = "";
+            if (countCorrectAnswer == 0)
+                diagnoz = "бедолага";
+            else if (countCorrectAnswer == 1)
+                diagnoz = "дурак";
+            else if (countCorrectAnswer == 2)
+                diagnoz = "дурик";
+            else if (countCorrectAnswer == 3)
+                diagnoz = "жить будешь";
+            else if (countCorrectAnswer == 4)
+                diagnoz = "тип";
+            else if (countCorrectAnswer == 5)
+                diagnoz = "тынг тип!!!";
+            return diagnoz;
+        }
+
+
     }
 }
