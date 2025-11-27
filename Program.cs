3119b9aedf11dfb9624186d5e61e52d8914a475a
stud <stud@10-600-09>
2025-11-27 11:35:16 +0300

    добавил функцию (Да Или Нет), сделал возможность сыграть еще раз

--- Program.cs
+++ Program.cs
@@ -11,20 +11,34 @@ namespace _12345
     {
         static void Main(string[] args)
         {
-
-            string[] question = { "четрые или пять?", "сколько ног у осьминога", "сколько пар завтра", "сколько время", "gg" };
+            // поправить названия функций и переменных
+            string[] question =
+            {
+                "четрые или пять?",
+                "сколько ног у осьминога",
+                "сколько пар завтра",
+                "сколько время",
+                "gg"
+            };
             string[] answer = { "5", "7", "4", "9:52", "dobeg" };
 
             int countCorrectAnswer = 0;
 
             Console.WriteLine("Как вас зовут?");
-
             string nameUser = Console.ReadLine();
+            Console.WriteLine($"Отлично, {nameUser}, отвечай на вопросы, и узнаешь свой диагноз");
+            Console.WriteLine("Готов ?");
+            string userMessege = Console.ReadLine();
 
-            Console.WriteLine($"Отлично, {nameUser} ,отвечай на вопросы, и узнаешь свой диагноз");
+            while (YesOrNo(userMessege) == false)
+            {
+                Console.WriteLine("Такого варианта нет. Готов?");
+
+                userMessege = Console.ReadLine();
+            }
 
             Console.WriteLine();
-            for (int i = 0; i < 5; i++) // сам тест 
+            for (int i = 0; i < question.Length; i++) // сам тест 
             {
                 Console.WriteLine(question[i]);
                 string userAnswer = Console.ReadLine();
@@ -33,17 +47,31 @@ namespace _12345
                     countCorrectAnswer++;
                 }
             }
-            string Diagnosis = DefinitionDiagnosis(countCorrectAnswer);
+
+            string diagnosis = DefinitDiagnosis(countCorrectAnswer);
+
             Console.WriteLine();
+
             Console.WriteLine($"Количество баллов: {countCorrectAnswer}");
-            Console.WriteLine($"{nameUser}, твой диагноз: {Diagnosis}");
+            Console.WriteLine($"{nameUser}, твой диагноз: {diagnosis}");
 
+            Console.WriteLine();
+
+            Console.WriteLine("Сыграем еще раз?");
+            userMessege = Console.ReadLine();
+            while (YesOrNo(userMessege) == false)
+            {
+                Console.WriteLine("непраильнвый ответ, давай еще раз");
+                Console.WriteLine("Сыграем еще раз?");
+                Console.WriteLine();
+                userMessege = Console.ReadLine();
+            }
         }
-        
 
 
 
-        static string DefinitionDiagnosis(int countCorrectAnswer)
+
+        static string DefinitDiagnosis(int countCorrectAnswer)
         {
             string diagnoz = "";
             if (countCorrectAnswer == 0)
@@ -61,6 +89,17 @@ namespace _12345
             return diagnoz;
         }
 
-
+        static bool YesOrNo(string messege)
+        {
+            if (messege == "да" || messege == "Да" || messege == "ДА")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
+
 }
